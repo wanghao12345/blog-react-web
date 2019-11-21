9957900ce8c1be6d2fd849dce87c33158a5f52c9
wanghao <1750110604@qq.com>
2019-11-21 18:04:22 +0800

    update

--- config/head.js
+++ config/head.js
@@ -1,11 +1,8 @@
 let dev = process.env.NODE_ENV !== 'production';
 export default {
-  remoteHost:dev?'http://localhost:7001/':'http://47.107.229.206:7001/', //请求API地址
-  backGroundImage:'/static/bg.jpg',       //网站背景图
   favicon:'/static/favicon.png',
-  keyWords:'徐浩的网站',                             //seo 关键词
-  websiteTitle:'徐浩的网站',
-  description:'徐浩的网站',
+  keyWords:'汪浩的博客网站',                             //seo 关键词
+  websiteTitle:'汪浩的博客网站',
+  description:'汪浩的博客网站',
   articleLimit:6,                               //文章显示列表的数量
-
 }
