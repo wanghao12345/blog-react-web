33c9f81fc7b755ff979322d3a7410378352ce11c
wanghao <1750110604@qq.com>
2019-11-29 15:35:10 +0800

    登录

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Form, Button, Checkbox, Input, Icon } from 'antd'
 import Layout from '../../components/Layout'
 import '../../static/assets/login.less'
 
@@ -8,17 +9,70 @@ class Login extends Component {
   }
 
   render () {
+    const { getFieldDecorator } = this.props.form;
     return (
       <Layout>
         <div className="login-wrapper">
-          登录
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入你的用户名!' }],
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Username"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入你的密码!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="Password"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: true,
+            })(<Checkbox>记住账号</Checkbox>)}
+            <a className="login-form-forgot" href="">
+              忘记密码
+            </a>
+            <Button 
+              type="primary" 
+              htmlType="submit" 
+              className="login-form-button"
+            >
+              登录
+            </Button>
+            或者 <a href="">注册!</a>
+          </Form.Item>
+        </Form>
         </div>
       </Layout>  
     )
   }
 
 
+  /**
+   * 登录
+   */
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
 }
 
+const LoginForm = Form.create({ name: 'normal_login' })(Login)
 
-export default Login
+export default LoginForm
